8dc669e0f61de8442c38995f5f18441ced0ce94e
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-15 18:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  const utcDate1 = Date.UTC(
+    parseInt(date1.substring(0, 4)),
+    parseInt(date1.substring(5, 7)) - 1,
+    parseInt(date1.substring(8))
+  );
+
+  const utcDate2 = Date.UTC(
+    parseInt(date2.substring(0, 4)),
+    parseInt(date2.substring(5, 7)) - 1,
+    parseInt(date2.substring(8)) 
+  );
+
+  const diffInMilliseconds = utcDate2 - utcDate1;
+  const diffInDays = diffInMilliseconds / (24 * 60 * 60 * 1000);
+
+  return diffInDays;
 };
 
-// Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
